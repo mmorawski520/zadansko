fa33f0feccfd0b3c13cacf261252f974d881fd18
jp2137 <sbaiyassine2004@gmail.com>
2022-09-09 11:09:36 +0200

    randomizer implementation

--- app/src/main/java/com/example/zadansko/MainActivity.kt
+++ app/src/main/java/com/example/zadansko/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.zadansko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.util.toRange
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    private var counter:Int=0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.  onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnIncrease = findViewById<Button>(R.id.btnIncrease)
+        val btnRandom = findViewById<Button>(R.id.btnRandom)
+
+        val textviewRandom = findViewById<TextView>(R.id.textViewRandom)
+        val textViewIncrease = findViewById<TextView>(R.id.textviewIncrease);
+        val random = Random()
+        btnIncrease.setOnClickListener {
+            counter++;
+            textViewIncrease.text = "increase : "+counter.toString()
+        }
+
+        btnRandom.setOnClickListener {
+            textviewRandom.text = "random "+  random.nextInt()
+        }
     }
 }
